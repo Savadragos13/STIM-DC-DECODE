f36b1c1b9275ed8e8943c6203d1da2daf6002dee
savadragos13 <savasavadragos@gmail.com>
2025-10-23 08:45:59 +0300

    pentru italieni

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Italia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Italia.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name= "Inceput")
-public class PrimulMers extends OpMode {
+@TeleOp(name= "Italy")
+public class Italia extends OpMode {
     DcMotor motor1,motor2,motor3,motor4 ;
 
     public void init() {
@@ -17,17 +17,15 @@ public class PrimulMers extends OpMode {
     }
 
     public void loop() {
-        float x = gamepad1.left_stick_x;
+        float x = gamepad2.left_stick_x;
         float y = gamepad1.left_stick_y;
-        float i = gamepad1.right_stick_x;
-        float j = gamepad1.right_stick_y;
         //1 ****** 2
         //**********   motoarele roților ordine
         //**********
         //3 ****** 4
         //motor.setPower i-a valori intre -1 si 1 unde cea mai mare este -1 cea mai mare viteza pe care o poate atinge motorul cu spatele invers la 1
         //deci avem două controllere: gamepad1 și gamepad2
-        if (Math.abs(gamepad1.left_stick_x) > 0.1) {
+        if (Math.abs(gamepad2.left_stick_x) > 0.1) {
             //robotul se mișcă spre dreapta si stanga
             motor1.setPower(-x);
             motor2.setPower(x);
@@ -42,14 +40,6 @@ public class PrimulMers extends OpMode {
             motor3.setPower(y);
             motor4.setPower(y);
             telemetry.addData("Daca este left stick y ", "robot merge fata si spate");
-        }
-
-        if (Math.abs(gamepad1.right_stick_x) > 0.1) {//robotul se invarte pe loc spre dreapta
-            motor1.setPower(-i);
-            motor2.setPower(-i);
-            motor3.setPower(-i);
-            motor4.setPower(-i);
-            telemetry.addData("Daca este right stick x ", "robot se invarte stanga si dreapta");
         }
 
         motor1.setPower(0);
